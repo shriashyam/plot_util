395271ff396ed8bf235931be2dcc794bdadb14ba
Shria Shyam <sshyam@Orion-login-2.HPC.MsState.Edu>
2023-07-12 14:21:22 -0500

    Change ferret syntax to cartopy

--- plot_util.py
+++ plot_util.py
@@ -1,20 +1,22 @@
-import os
-import netCDF4 as nc
-import numpy.ma as ma
 import numpy as np
+import xarray as xr
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import matplotlib.pyplot as plt
+import sys
 
 def plot_util(control_file, expr_file, timeseries = None, var_name = "", lat = None, lon = None, X = None, Y = None):
 	
 	# if control_file exists and is netCDF, extract control dataset as masked array, else return with error message
 	try:
-		control_ds = nc.Dataset(control_file)
+		control_ds = xr.open_dataset(control_file)
 	except:
 		print("Control file not found.")
 		return
 
 	# if expr_file exists and is netCDF, extract experimental dataset as masked array, else return with error message
 	try:
-		expr_ds = nc.Dataset(expr_file)
+		expr_ds = xr.open_dataset(expr_file)
 	except:
 		print("Experimental file not found.")
 		return
@@ -26,36 +28,47 @@ def plot_util(control_file, expr_file, timeseries = None, var_name = "", lat = N
 		# plot_util control_file expr_file: list all common variables in the two files
 		print("The following variables are found in both control and experimental datasets: ")
 		print(*common_vars,sep=', ')
+		return
 	elif timeseries == None:
 		# plot_util control_file expr_file var_name: make three plots with a variable (var_name)
 		
 		# check if variable (var_name) is in both datasets, else return with error message
 		# if variable (var_name) is in both datasets, plot control, experimental, difference
 		if var_name in common_vars:
-			f = open("Plots.jnl", "w")
-			# control graph
-			f.write("set data " + control_file + " \n")
-			f.write("shade/title = \"Control " + var_name + "\" " + var_name + "; go land \n")
-			f.write("frame/file=CONTROL" + var_name + ".gif \n")
-			# expr graph
-			f.write("set data " + expr_file + " \n")
-			f.write("shade/title = \"Experimental " + var_name + "\" " + var_name + "; go land \n")
-			f.write("frame/file=EXPR" + var_name + ".gif \n")
-			# difference graph
-			os.system("cp " + control_file + " control_edited.nc")
-			os.system("ncrename -v " + var_name + ",old" + var_name + " control_edited.nc")
-			f.write("use control_edited.nc \n")
-			f.write("save/file=concat.cdf old" + var_name + "\n")
-			f.write("use " + expr_file + "\n")
-			f.write("save/append/file=concat.cdf " + var_name + "\n")
-			f.write("set data concat.cdf \n")
-			f.write("shade/palette=cmocean_balance/title = \"Difference (exp - con) in " + var_name + "\" " + var_name + "-old" + var_name + "; go land \n")
-			f.write("frame/file=DIFF" + var_name + ".gif \n")
-			# plot and reset
-			f.close()
-			os.system("ferret -script Plots.jnl")
-			os.system("rm Plots.jnl")	
-			os.system("rm control_edited.nc")
-			os.system("rm concat.cdf")
+			# check if latitude and longitude in both datasets, else return with error message
+			if "lat" in common_vars and "lon" in common_vars:
+				# control graph
+				fig = plt.figure()
+				ax = plt.axes(projection=ccrs.PlateCarree())
+				plt.contourf(control_ds["lon"], control_ds["lat"], np.squeeze(control_ds[var_name]), transform = ccrs.PlateCarree())
+				ax.add_feature(cfeature.COASTLINE)				
+				cbar = plt.colorbar()
+				cbar.set_label(var_name)
+				plt.title("Control " + var_name)
+				plt.savefig(var_name + 'control.png')
+				# expr graph
+				fig = plt.figure()
+				ax = plt.axes(projection=ccrs.PlateCarree())
+				plt.contourf(expr_ds["lon"], expr_ds["lat"], np.squeeze(expr_ds[var_name]), transform = ccrs.PlateCarree())
+				ax.add_feature(cfeature.COASTLINE)
+				cbar = plt.colorbar()
+				cbar.set_label(var_name)
+				plt.title("Experimental " + var_name)
+				plt.savefig(var_name + 'expr.png')
+				#difference graph
+				fig = plt.figure()
+				ax = plt.axes(projection=ccrs.PlateCarree())
+				plt.contourf(control_ds["lon"], control_ds["lat"], np.squeeze(expr_ds[var_name]-control_ds[var_name]), transform = ccrs.PlateCarree())
+				ax.add_feature(cfeature.COASTLINE)
+				cbar = plt.colorbar()
+				cbar.set_label(var_name)
+				plt.title("Difference (experimental - control) " + var_name)
+				plt.savefig(var_name + 'difference.png')	
+			else:
+				print("Latitude and longitude not found in control and experimental datasets.")
+				return
 		else:
 			print(var_name + " not in both control and experimental datasets.")
+			return
+
+plot_util("/work2/noaa/nems/sshyam/comparisons/base/sfcf024.nc", "/work2/noaa/nems/sshyam/comparisons/noise1/sfcf024.nc", var_name = "ugrd10m")
