2e8a1a77ea1d59fb3d25645e510c96773d4a50e6
Shria Shyam <sshyam@Orion-login-4.HPC.MsState.Edu>
2023-07-14 11:25:11 -0500

    Add functions

--- plot_util.py
+++ plot_util.py
@@ -5,6 +5,34 @@ import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
 import numpy.ma as ma
 
+def plot_map(control_ds, expr_ds, var_name):
+	# plot_util control_file expr_file var_name: make three plots (experimental, control, and difference) plots with a variable var_name
+	data = np.array([np.squeeze(control_ds[var_name]), np.squeeze(expr_ds[var_name]), np.squeeze(expr_ds[var_name]-control_ds[var_name])])
+	titles = np.array(["Control", "Experimental", "Difference"])
+	for i in range(3):
+		fig = plt.figure()
+		ax = plt.axes(projection=ccrs.PlateCarree())
+		plt.contourf(control_ds["lon"], control_ds["lat"], data[i,:,:], transform=ccrs.PlateCarree())
+		ax.add_feature(cfeature.COASTLINE)
+		cbar = plt.colorbar()
+		cbar.set_label(var_name)
+		plt.title(titles[i] + " " + var_name)
+		plt.savefig(var_name + titles[i] + ".png")
+	return
+
+def plot_timeseries(control_ds, expr_ds, var_name, X, Y):
+	data = np.array([xr.DataArray.to_numpy(control_ds[var_name])[:,Y,X],xr.DataArray.to_numpy(expr_ds[var_name])[:,Y,X],xr.DataArray.to_numpy(expr_ds[var_name])[:,Y,X]-xr.DataArray.to_numpy(control_ds[var_name][:,Y,X])])
+	titles = np.array(["Control", "Experimental", "Difference"])
+	for i in range(3):
+		plt.figure()
+		plt.plot(xr.DataArray.to_numpy(control_ds["time"]),data[i,:])
+		plt.xlabel("Time")
+		plt.ylabel(var_name)
+		plt.title("Change Over Time in " + titles[i] + " " + var_name + " at Latitude " + str(round(xr.DataArray.to_numpy(control_ds["lat"])[Y,0])) + " and Longitude " + str(round(xr.DataArray.to_numpy(control_ds["lon"])[0,X])))
+		plt.savefig(var_name + titles[i] + "timeseries.png")
+	return
+
+
 def plot_util(control_file, expr_file, var_name = "", lat = None, lon = None, X = None, Y = None):
 	
 	# if control_file exists and is netCDF, extract control dataset as masked array, else return with error message
@@ -33,24 +61,13 @@ def plot_util(control_file, expr_file, var_name = "", lat = None, lon = None, X 
 		# check if variable (var_name) is in both datasets, else return with error message
 		print(var_name + " not in both control and experimental datasets.")
 		return
-	elif "lat" not in common_vars and "lon" not in common_vars:
+	elif "lat" not in common_vars or "lon" not in common_vars:
 		# check if latitude and longitude in both datasets, else return with error message
 		print("Latitude and longitude not found in control and experimental datasets.")
 		return
 	elif len(np.squeeze(control_ds[var_name]).shape) == 2 or len(np.squeeze(expr_ds[var_name]).shape) == 2:
-		# plot_util control_file expr_file var_name: make three plots with a variable (var_name)
-		# make experimental, control, and difference plots
-		data = np.array([np.squeeze(control_ds[var_name]), np.squeeze(expr_ds[var_name]), np.squeeze(expr_ds[var_name]-control_ds[var_name])])
-		titles = np.array(["Control", "Experimental", "Difference"])
-		for i in range(3):
-			fig = plt.figure()
-			ax = plt.axes(projection=ccrs.PlateCarree())
-			plt.contourf(control_ds["lon"], control_ds["lat"], data[i, :, :], transform = ccrs.PlateCarree())
-			ax.add_feature(cfeature.COASTLINE)
-			cbar = plt.colorbar()
-			cbar.set_label(var_name)
-			plt.title(titles[i] + " " + var_name)
-			plt.savefig(var_name + titles[i] + ".png")
+		# plot_util control_file expr_file var_name: make three plots (experimental, control, and difference) with a variable (var_name)
+		plot_map(control_ds, expr_ds, var_name)
 		return
 	else:
 		if not lat == None:
@@ -60,15 +77,6 @@ def plot_util(control_file, expr_file, var_name = "", lat = None, lon = None, X 
 		if X == None or Y == None:
 			print("Provide X/Y or latitude/longitude values.")
 			return
-		data = np.array([xr.DataArray.to_numpy(control_ds[var_name])[:,Y,X],xr.DataArray.to_numpy(expr_ds[var_name])[:,Y,X],xr.DataArray.to_numpy(expr_ds[var_name])[:,Y,X]-xr.DataArray.to_numpy(control_ds[var_name][:,Y,X])])
-		titles = np.array(["Control", "Experimental", "Difference"])
-		for i in range(3):
-			plt.figure()
-			plt.plot(xr.DataArray.to_numpy(control_ds["time"]),data[i,:])
-			plt.xlabel("Time")
-			plt.ylabel(var_name)
-			plt.title("Change Over Time in " + titles[i] + " " + var_name + " at Latitude " + str(round(xr.DataArray.to_numpy(control_ds["lat"])[Y,0])) + " and Longitude " + str(round(xr.DataArray.to_numpy(control_ds["lon"])[0,X])))
-			plt.savefig(var_name + titles[i] + "timeseries.png")
+		plot_timeseries(control_ds, expr_ds, var_name, X, Y)
 		return
-plot_util("sfccat.nc", "sfccat.nc", var_name = "tmp2m", lon = 23, lat = -36)
 
